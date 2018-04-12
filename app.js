e48282f1efb14763972b69d9d355d79cb37bdd56
BroTom <brodi.tomicic@gmail.com>
2018-04-12 16:03:31 -0600

    create joke generator

---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready ( function() {
+    $.ajax({
+        url: 'https://icanhazdadjoke.com/',
+        method: 'GET', 
+        dataType: 'JSON'
+    }).done( function(res) {
+        $('#jokeDisplay').text(res.joke)
+        console.log(res.joke)
+    }).fail( function(error) {
+        alert(error.responseJSON.errors)
+    })
+    $('#nextJoke').on('click', function() {
+        $.ajax({
+            url: 'https://icanhazdadjoke.com/',
+            method: 'GET', 
+            dataType: 'JSON'
+        }).done( function(res) {
+            $('#jokeDisplay').text(res.joke)
+            console.log(res.joke)
+        }).fail( function(error) {
+            alert(error.responseJSON.errors)
+        })
+    })
+})
